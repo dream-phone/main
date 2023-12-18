166071cd16c25113dbf60fd656d3f39fec96131c
enkhtugs <tugsbaby@MacBook-Pro.local>
2023-12-18 17:25:48 +0800

    core files

---
+++ src/app/Iphone/page.js
@@ -0,0 +1,46 @@
+export default function Home(){
+    return(
+        <div className="bigIphoneContainer">
+            <div className="container">
+            <img src="iphone15promax.jpeg" className="picture"></img>
+            
+         </div>
+         <div className="container">
+            <img src="iphone15promax.jpeg" className="picture"></img>
+            
+         </div>
+         <div className="container">
+            <img src="iphone15promax.jpeg" className="picture"></img>
+            
+         </div>
+         <div className="container">
+            <img src="iphone15promax.jpeg" className="picture"></img>
+            
+         </div>
+         <div className="container">
+            <img src="iphone15promax.jpeg" className="picture"></img>
+            
+         </div>
+         <div className="container">
+            <img src="iphone15promax.jpeg" className="picture"></img>
+            
+         </div>
+         <div className="container">
+            <img src="iphone15promax.jpeg" className="picture"></img>
+            
+         </div>
+         <div className="container">
+            <img src="iphone15promax.jpeg" className="picture"></img>
+            
+         </div>
+         <div className="container">
+            <img src="iphone15promax.jpeg" className="picture"></img>
+            
+         </div>           <div className="container">
+            <img src="iphone15promax.jpeg" className="picture"></img>
+            
+         </div>
+
+        </div>
+    )
+}
