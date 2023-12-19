e39a2a25bdd2d6f0e17cb7dcce41760e099f11f2
enkhtugs <tugsbaby@MacBook-Pro.local>
2023-12-19 21:19:40 +0800

    za bz yhu

--- src/app/Iphone/page.js
+++ src/app/Iphone/page.js
@@ -1,45 +1,12 @@
-export default function Home(){
+export default function Home(Name){
     return(
         <div className="bigIphoneContainer">
             <div className="container">
+
             <img src="iphone15promax.jpeg" className="picture"></img>
-            
+          
          </div>
-         <div className="container">
-            <img src="iphone15promax.jpeg" className="picture"></img>
-            
-         </div>
-         <div className="container">
-            <img src="iphone15promax.jpeg" className="picture"></img>
-            
-         </div>
-         <div className="container">
-            <img src="iphone15promax.jpeg" className="picture"></img>
-            
-         </div>
-         <div className="container">
-            <img src="iphone15promax.jpeg" className="picture"></img>
-            
-         </div>
-         <div className="container">
-            <img src="iphone15promax.jpeg" className="picture"></img>
-            
-         </div>
-         <div className="container">
-            <img src="iphone15promax.jpeg" className="picture"></img>
-            
-         </div>
-         <div className="container">
-            <img src="iphone15promax.jpeg" className="picture"></img>
-            
-         </div>
-         <div className="container">
-            <img src="iphone15promax.jpeg" className="picture"></img>
-            
-         </div>           <div className="container">
-            <img src="iphone15promax.jpeg" className="picture"></img>
-            
-         </div>
+         
 
         </div>
     )
