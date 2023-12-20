d26d6692d9dd3d96b533242f9a0edb990e3d665d
enkhtugs <tugsbaby@MacBook-Pro.local>
2023-12-20 22:53:37 +0800

    ehnii odriihoo

--- src/app/components/Data.jsx
+++ src/app/components/Data.jsx
@@ -1,41 +1,47 @@
 export const Data = [
   {
-    path: "387488356_1051399176066774_3542605451090541901_n.jpg",
-  },
-  {
-    path: "387494429_374326098295073_4553929541408022140_n.jpg",
-  },
-  {
-    path: "387502229_1879511792467576_7495501980744649851_n.jpg",
-  },
-  {
-    path: "387502242_347721044566760_1775894401523770558_n.jpg",
-  },
-  {
-    path: "iphone13.webp",
-  },
-  {
-    path: "iphone13promax.jpeg",
-  },
-  {
-    path: "iphone14.jpeg",
-  },
-  {
-    path: "iphone14pro.png",
-  },
-  {
-    path: "iphone14promax.webp",
+    path: "iphone15promax.webp",
   },
   {
     path: "iphone15pro.webp",
   },
   {
-    path: "iphone15promax.jpeg",
+    path: "iphone15.jpeg",
   },
   {
-    path: "iphone15promax.webp",
+    path: "iphone14promax.webp",
   },
   {
-    path: "s22ultra.webp",
+    path: "iphone14pro.png",
+  },
+  {
+    path: "iphone14.jpeg",
+  },
+  {
+    path: "iphone13promax.jpeg",
+  },
+  {
+    path: "iphone13pro.png",
+  },
+  {
+    path: "iphone13.webp",
+  },
+  {
+    path: "iphone12promax.jpeg",
+  },
+  {
+    path: "iphone12pro.jpeg",
+  },
+  {
+    path: "iphone12.jpeg",
+  },
+  {
+    path: "iphoneXmax.jpeg",
+  },
+  {
+    path: "iphoneXryellowone.jpeg",
+  },
+  {
+    path: "iphoneXS.jpeg",
   },
 ];
