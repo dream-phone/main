f4095fd53f683ba62f2f4e8ef7b6a1dba8d421ec
bold ariunbold <ariunboldbold200@gmail.com>
2023-12-21 16:03:30 +0800

    image support

--- src/app/Iphone/page.js
+++ src/app/Iphone/page.js
@@ -3,18 +3,23 @@ import Image from "next/image";
 //100% tailwind ashiglare
 export default function Home(Name) {
   return (
-    <div className="flex w-[100vw] h-[100vh]">
-      <div className="flex-wrap justify-center flex gap-[10px]">
+    <div className="flex w-[100vw] h-[100vh] justify-center overflow-x-hidden">
+      <div className="flex-wrap  flex gap-[10px] justify-center">
         {Data.map((element, index) => {
           return (
-            <div key={index} className="w-[300px] h-[500px] relative">
+            <div
+              key={index}
+              className="w-[15%] max-[600px]:w-[30%] max-[700px]:w-[40%] max-[600px]:h-[auto] max-[700px]:h-[40%] h-[auto] text-center"
+            >
+              <div className="w-[100%] h-[90%] relative">
+                <Image
+                  sizes="100vh"
+                  fill
+                  alt="phone image"
+                  src={element.path}
+                ></Image>
+              </div>
               {/* Image compoenet htmliin img tagaas hmq udaan ucraas soliv */}
-              <Image
-                layout="fill"
-                sizes="auto"
-                alt="phone image"
-                src={element.path}
-              ></Image>
               <h1>{element.name}</h1>
             </div>
           );
