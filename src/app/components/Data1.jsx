42ae7c3ea8a3b70c684b4ebcf1621166a5433256
enkhtugs <tugsbaby@MacBook-Pro.local>
2023-12-22 20:34:13 +0800

    zuraga medeleltge oruultsn

---
+++ src/app/components/Data1.jsx
@@ -0,0 +1,94 @@
+export const Data1 = [
+  {
+    path: "/iphone15promax.webp",
+    name: "iphone 15 pro max",
+    medeelel: "",
+    price: "",
+  },
+  {
+    path: "/iphone15pro.webp",
+    name: "iphone 15 pro",
+    medeelel: "",
+    price: "",
+  },
+
+  //   {
+  //     path: "/iphone14promax.webp",
+  //     name: "iphone 14 pro max",
+  //     medeelel: "",
+  //     price: "",
+  //   },
+  //   {
+  //     path: "/iphone14pro.png",
+  //     name: "iphone 14 pro",
+  //     medeelel: "",
+  //     price: "",
+  //   },
+  //   {
+  //     name: "iphone 14",
+  //     path: "/iphone14.jpeg",
+  //     medeelel: "",
+  //     price: "",
+  //   },
+  {
+    path: "/iphone13promax.jpeg",
+    name: "iphone 13 pro max",
+    medeelel: "memory:128gb",
+    price: "2.500.000",
+  },
+  //   {
+  //     path: "/iphone13pro.png",
+  //     name: "iphone 13 pro",
+  //     medeelel: "",
+  //     price: "",
+  //   },
+  {
+    path: "/iphone13.webp",
+    name: "iphone 13 (128gb)",
+    medeelel: " memory:128gb",
+    price: "1.950.000",
+  },
+  {
+    path: "/iphone12promax.jpeg",
+    name: "iphone 12 pro max",
+    medeelel: " memory:128gb",
+    price: "",
+  },
+  //   {
+  //     path: "/iphone12pro.jpeg",
+  //     name: "iphone 12 pro",
+  //     medeelel: "",
+  //     price: "",
+  //   },
+  {
+    path: "/iphone12.jpeg",
+    name: "iphone 12 ",
+    medeelel: " memory:64gb",
+    price: " 1.450.000",
+  },
+  {
+    path: "/iphone11.png",
+    name: "iphone 11",
+    medeelel: "",
+    price: "",
+  },
+
+  {
+    path: "/iphoneXmax.jpeg",
+    name: "iphone Xsmax",
+    medeelel: "memory:64gb",
+    price: "1.150.000",
+  },
+  {
+    path: "/iphoneXryellowone.jpeg",
+    name: "iphone XR (yellow)",
+    medeelel: "memory:64gb",
+    price: "850.000",
+  },
+  //   {
+  //     path: "/iphoneXS.jpeg",
+  //     name: "iphone XS",
+  //     medeelel: "",
+  //     price: "750.000",
+  //   },
+];
--- src/app/redmi/page.js
+++ src/app/redmi/page.js
@@ -1,3 +1,32 @@
-export default function Home() {
-  return <div>welcome to redmi page</div>;
+import { Data } from "../components/Data";
+import Image from "next/image";
+//100% tailwind ashiglare
+export default function Home(Name) {
+  return (
+    <div className="flex w-[100vw] h-[100vh] justify-center overflow-x-hidden">
+      <div className="flex-wrap  flex gap-[10px] justify-center">
+        {Data.map((element, index) => {
+          return (
+            <div
+              key={index}
+              className="w-[15%] max-[600px]:w-[30%] max-[700px]:w-[40%] max-[600px]:h-[auto] max-[700px]:h-[40%] h-[auto] text-center flex-col "
+            >
+              <div className="w-[100%] h-[90%] relative">
+                <Image
+                  sizes="100vh"
+                  fill
+                  alt="phone image"
+                  src={element.path}
+                ></Image>
+              </div>
+              {/* Image compoenet htmliin img tagaas hmq udaan ucraas soliv */}
+              <h1>{element.name}</h1>
+              <h1>{element.medeelel}</h1>
+              <h1>{element.price}</h1>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
