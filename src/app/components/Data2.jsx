e2898801ec439b8c57a7a630723317fabe56354f
enkhtugs <tugsbaby@MacBook-Pro.local>
2023-12-23 10:15:34 +0800

    odo boloh bailgude

---
+++ src/app/components/Data2.jsx
@@ -0,0 +1,94 @@
+export const Data = [
+  {
+    path: "/a90.jpeg",
+    name: "Samsung A90",
+    medeelel: "memory:",
+    price: "",
+  },
+  {
+    path: "/a71.jpeg",
+    name: "Samsung A71 (5G)",
+    medeelel: "",
+    price: "",
+  },
+
+  {
+    path: "/iphone14promax.webp",
+    name: "iphone 14 pro max",
+    medeelel: "",
+    price: "",
+  },
+  {
+    path: "/iphone14pro.png",
+    name: "iphone 14 pro",
+    medeelel: "",
+    price: "",
+  },
+  {
+    name: "iphone 14",
+    path: "/iphone14.jpeg",
+    medeelel: "",
+    price: "",
+  },
+  {
+    path: "/iphone13promax.jpeg",
+    name: "iphone 13 pro max",
+    medeelel: "",
+    price: "",
+  },
+  {
+    path: "/iphone13pro.png",
+    name: "iphone 13 pro",
+    medeelel: "",
+    price: "",
+  },
+  {
+    path: "/iphone13.webp",
+    name: "iphone 13",
+    medeelel: "",
+    price: "",
+  },
+  {
+    path: "/iphone12promax.jpeg",
+    name: "iphone 12 pro max",
+    medeelel: "",
+    price: "",
+  },
+  {
+    path: "/iphone12pro.jpeg",
+    name: "iphone 12 pro",
+    medeelel: "",
+    price: "",
+  },
+  {
+    path: "/iphone12.jpeg",
+    name: "iphone 12",
+    medeelel: "",
+    price: "",
+  },
+  {
+    path: "/iphone11.png",
+    name: "iphone 11",
+    medeelel: "",
+    price: "",
+  },
+
+  {
+    path: "/iphoneXmax.jpeg",
+    name: "iphone Xsmax",
+    medeelel: "",
+    price: "1.000.000",
+  },
+  {
+    path: "/iphoneXryellowone.jpeg",
+    name: "iphone XR (yellow)",
+    medeelel: "",
+    price: "800.000",
+  },
+  {
+    path: "/iphoneXS.jpeg",
+    name: "iphone XS",
+    medeelel: "",
+    price: "750.000",
+  },
+];
